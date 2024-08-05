3ab4a13f891eff4b129d942c5e5f74c5e100805e
DeepaK <dvar665011@gmail.com>
2024-08-05 23:12:15 +0530

    uodated count owels

--- DAY3/Functions/vowels-in-a-string.js
+++ DAY3/Functions/vowels-in-a-string.js
@@ -1,3 +1,16 @@
 const countvowels=(str)=>{
-    
-}
+let count=0;
+let vowels='aeiou'
+let bigvowels=vowels.toUpperCase();
+   for (let i = 0; i < str.length; i++) {
+  for (let j = 0; j < vowels.length; j++) {
+    if(str.charAt(i)===vowels.charAt(j)||str.charAt(i)===bigvowels.charAt(j))
+        count++;
+  }
+  }
+  return count;
+  
+ }
+let string="aeiou"
+
+console.log(countvowels(string));
